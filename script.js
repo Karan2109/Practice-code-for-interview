2edd8c2701264806a31009086d69df790288c0aa
Karan Singh <109736639+Karan2109@users.noreply.github.com>
2025-01-08 01:15:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// reversal of the string
 let newStr = "Hello, World!";
 
-console.log(newStr);
 function reverseString(str) {
     return str.split("").reverse().join("");
 }
 console.log(reverseString(newStr));
+
+
